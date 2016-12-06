459808fef29171df3ef92510aad17e7e7b69093f
Gustavo Berman <gustavoberman@gmail.com>
2016-12-06 16:49:21 -0300

    agrego los vlores en un recurso de string-array selecciono el recurso y elijo al azar de ese string array

--- app/src/main/java/com/example/android/mylegocreationary/MainActivity.java
+++ app/src/main/java/com/example/android/mylegocreationary/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.android.mylegocreationary;
 
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void cambiarMensaje(View view) {
+        Resources res = getResources();
+        String[] animal = res.getStringArray(R.array.animals_array);
+        String randomStr = animal[new Random().nextInt(animal.length)];
+        TextView texto = (TextView) findViewById(R.id.mensaje);
+        texto.setText(randomStr);
+    }
+
 }
