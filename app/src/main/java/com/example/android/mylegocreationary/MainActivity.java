7759552ea0fbc19841be70cc155943e590f46367
Gustavo Berman <gustavoberman@gmail.com>
2016-12-14 11:22:16 -0300

    agregados land-layout y saveInstance para no perder los valores del dado y de la carta al rotar la pantalla

--- app/src/main/java/com/example/android/mylegocreationary/MainActivity.java
+++ app/src/main/java/com/example/android/mylegocreationary/MainActivity.java
@@ -22,6 +22,9 @@ public class MainActivity extends AppCompatActivity {
     private int[] dadoArray = {R.drawable.dice1, R.drawable.dice2, R.drawable.dice3, R.drawable.dice4, R.drawable.dice5, R.drawable.dice6};
     private int[] cartasArray = {R.drawable.lego_03, R.drawable.lego_04, R.drawable.lego_05, R.drawable.lego_06, R.drawable.lego_07, R.drawable.lego_08, R.drawable.lego_09, R.drawable.lego_10, R.drawable.lego_11, R.drawable.lego_12, R.drawable.lego_13, R.drawable.lego_14, R.drawable.lego_15, R.drawable.lego_16, R.drawable.lego_17, R.drawable.lego_18, R.drawable.lego_19, R.drawable.lego_20, R.drawable.lego_21, R.drawable.lego_22, R.drawable.lego_23, R.drawable.lego_24, R.drawable.lego_25, R.drawable.lego_26, R.drawable.lego_27, R.drawable.lego_28, R.drawable.lego_29, R.drawable.lego_30, R.drawable.lego_31, R.drawable.lego_32, R.drawable.lego_33};
 
+    int rDado, rCarta;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,15 +32,32 @@ public class MainActivity extends AppCompatActivity {
 
         //load dice sound
         sound_id = dice_sound.load(this, R.raw.shake_dice, 1);
+
+        //recuperar el estado si viene de una vista anterior
+        if (savedInstanceState != null) {
+            rDado = savedInstanceState.getInt("dado");
+            rCarta = savedInstanceState.getInt("carta");
+            ImageView mostrarDado = (ImageView) findViewById(R.id.dado);
+            ImageView mostrarCarta = (ImageView) findViewById(R.id.carta);
+            mostrarDado.setImageResource(dadoArray[rDado]);
+            mostrarCarta.setImageResource(cartasArray[rCarta]);
+        }
+    }
+
+    //Si la pantalla la vista se tiene que destruir, guardar el estado del dado y de la carta
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        savedInstanceState.putInt("dado", rDado);
+        savedInstanceState.putInt("carta", rCarta);
     }
 
     public void lanzarDado(View view) {
-
         ImageView mostrarDado = (ImageView) findViewById(R.id.dado);
         ImageView mostrarCarta = (ImageView) findViewById(R.id.carta);
 
-        int rDado = new Random().nextInt(dadoArray.length);
-        int rCarta = new Random().nextInt(cartasArray.length);
+        //Seleccionar un lado del dado y una carta al azar
+        rDado = new Random().nextInt(dadoArray.length);
+        rCarta = new Random().nextInt(cartasArray.length);
 
         //Start rolling sound
         dice_sound.play(sound_id, 1.0f, 1.0f, 0, 0, 1.0f);
@@ -46,11 +66,9 @@ public class MainActivity extends AppCompatActivity {
         Animation rotate = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.rotate);
         mostrarDado.startAnimation(rotate);
         //Y mostrar una imagen al azar
-        //mostrarDado.setImageResource(dadoArray[new Random().nextInt(dadoArray.length)]);
         mostrarDado.setImageResource(dadoArray[rDado]);
         //lo mismo con la carta
         mostrarCarta.startAnimation(rotate);
-        //mostrarCarta.setImageResource(cartasArray[new Random().nextInt(cartasArray.length)]);
         mostrarCarta.setImageResource(cartasArray[rCarta]);
 
 
