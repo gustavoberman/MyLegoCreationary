c7ff05f8a6fe86a748a482958c8a8e22d83020d0
Gustavo Berman <gustavoberman@gmail.com>
2016-12-21 12:58:45 -0300

    Agregado el menu Agregadas las reglas  Agregado un tts para describir el dado

--- app/src/main/java/com/example/android/mylegocreationary/MainActivity.java
+++ app/src/main/java/com/example/android/mylegocreationary/MainActivity.java
@@ -1,16 +1,21 @@
 package com.example.android.mylegocreationary;
 
+import android.content.Intent;
 import android.content.res.Resources;
 import android.graphics.drawable.Drawable;
 import android.media.AudioManager;
 import android.media.SoundPool;
+import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Random;
 
@@ -21,8 +26,10 @@ public class MainActivity extends AppCompatActivity {
     int sound_id;        //Used to control sound stream return by SoundPool
     private int[] dadoArray = {R.drawable.dice1, R.drawable.dice2, R.drawable.dice3, R.drawable.dice4, R.drawable.dice5, R.drawable.dice6};
     private int[] cartasArray = {R.drawable.lego_03, R.drawable.lego_04, R.drawable.lego_05, R.drawable.lego_06, R.drawable.lego_07, R.drawable.lego_08, R.drawable.lego_09, R.drawable.lego_10, R.drawable.lego_11, R.drawable.lego_12, R.drawable.lego_13, R.drawable.lego_14, R.drawable.lego_15, R.drawable.lego_16, R.drawable.lego_17, R.drawable.lego_18, R.drawable.lego_19, R.drawable.lego_20, R.drawable.lego_21, R.drawable.lego_22, R.drawable.lego_23, R.drawable.lego_24, R.drawable.lego_25, R.drawable.lego_26, R.drawable.lego_27, R.drawable.lego_28, R.drawable.lego_29, R.drawable.lego_30, R.drawable.lego_31, R.drawable.lego_32, R.drawable.lego_33};
-
+    String[] dadoNombre = {"Elegir categoría", "Cosas", "Naturaleza", "Puntos dobles", "Edificios", "Vehículos"};
     int rDado, rCarta;
+
+    TextToSpeech t1;
 
 
     @Override
@@ -32,6 +39,19 @@ public class MainActivity extends AppCompatActivity {
 
         //load dice sound
         sound_id = dice_sound.load(this, R.raw.shake_dice, 1);
+
+        //tts
+        t1 = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status != TextToSpeech.ERROR) {
+                    //String resultado = t1.getLanguage().toString();
+                    //String resultado = (String) status;
+                    //Toast.makeText(getApplicationContext(), resultado, Toast.LENGTH_SHORT).show();
+                    //t1.setLanguage(Locale.es_ES);
+                }
+            }
+        });
 
         //recuperar el estado si viene de una vista anterior
         if (savedInstanceState != null) {
@@ -49,6 +69,27 @@ public class MainActivity extends AppCompatActivity {
     public void onSaveInstanceState(Bundle savedInstanceState) {
         savedInstanceState.putInt("dado", rDado);
         savedInstanceState.putInt("carta", rCarta);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //Inflate menu
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_reglas:
+                //code to run
+                Intent intent = new Intent(this, ReglasActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     public void lanzarDado(View view) {
@@ -71,8 +112,18 @@ public class MainActivity extends AppCompatActivity {
         mostrarCarta.startAnimation(rotate);
         mostrarCarta.setImageResource(cartasArray[rCarta]);
 
+        //nombrar resultado dado
+        t1.speak(dadoNombre[rDado], TextToSpeech.QUEUE_FLUSH, null);
 
     }
 
+    //tts
+    public void onPause() {
+        if (t1 != null) {
+            t1.stop();
+            t1.shutdown();
+        }
+        super.onPause();
+    }
 
 }
